7627d454cab91460913f42aa19fd4cfd85090667
GSacata <gsacata@gmail.com>
2024-02-24 18:20:07 -0300

    get method implemented

--- backend/todolist/views.py
+++ backend/todolist/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+# from django.shortcuts import render
+# from rest_framework import viewsets
+# from rest_framework.response import Response
+
+# from todolist.models import Task
+# from todolist.serializers import TaskSerializer
+# # Create your views here.
+
+# class TaskViewSet(viewsets.ModelViewSet):
+#     def list(self, request):
+#         queryset = Task.objects.all()
+#         serializer = TaskSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from todolist.models import Task
 from todolist.serializers import TaskSerializer
-# Create your views here.
+...
 
-class TaskViewSet(viewsets.ModelViewSet):
-    def list(self, request):
-        queryset = Task.objects.all()
-        serializer = TaskSerializer(queryset, many=True)
-        return Response(serializer.data)
+@api_view(['GET'])
+def task_list(request):
+    tasklist = Task.objects.all()
+    serializer = TaskSerializer(tasklist, many=True)
+    return Response(serializer.data)
