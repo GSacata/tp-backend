ca4201d3f2b5efbe61ff4949bce927ec7f71f92e
GSacata <gsacata@gmail.com>
2024-02-25 15:42:09 -0300

    put and delete methods working

--- backend/todolist/urls.py
+++ backend/todolist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todolist.views import task_list
+from todolist.views import task_list, task_detail_change_delete
 
 urlpatterns = [
-    path('', task_list)
+    path('', task_list),
+    path('<int:pk>/', task_detail_change_delete)
 ]
--- backend/todolist/views.py
+++ backend/todolist/views.py
@@ -34,4 +34,25 @@ def task_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def task_detail_change_delete(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data) # compara conteúdo 'task' com 'serializer.data'
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # retorna erro, depois o status?
+    
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
